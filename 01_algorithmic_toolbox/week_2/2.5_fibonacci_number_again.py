9b08a1869e14e175665a3f88235767b0b0b34fd9
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2021-01-11 23:29:51 +0300

    Added 2.5_fibonacci_number_again.py, in the middle of solving 2.6_last_digit_of_sum_of_Fib_numbers.py

--- 01_algorithmic_toolbox/week_2/2.5_fibonacci_number_again.py
+++ 01_algorithmic_toolbox/week_2/2.5_fibonacci_number_again.py
@@ -8,12 +8,25 @@ def get_Fib(n):
     return second
 
 
-def get_pisano_period():
-    pass
+def get_pisano_period(m):
+    first, second = 0, 1
+    for i in range(m ** 2):
+        first, second = second, (first + second) % m
+        if (first, second) == (0, 1):
+            return i + 1
+
+
+def get_remainder_Fib_N(n, m):
+    pisano_period = get_pisano_period(m)
+    idx = n % pisano_period
+    return get_Fib(idx) % m
 
 
 def main():
     n, m = map(int, input().split())
+    # print(f'Pisano period for modulo {m} = {get_pisano_period(m)}')
+    # print(f'F({n})mod{m} = {get_remainder_Fib_N(n, m)}')
+    print(get_remainder_Fib_N(n, m))
 
 
 if __name__ == '__main__':
