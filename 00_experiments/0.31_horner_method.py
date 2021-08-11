52c5290fc89a377154b1fa56541e8e2c36ccf368
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2021-08-11 20:05:58 +0300

    Added implementation of Rabin-Karp algorithm for pattern matching problem.

--- 00_experiments/0.31_horner_method.py
+++ 00_experiments/0.31_horner_method.py
@@ -1,5 +1,12 @@
 # evaluates polynomial specified by its 'coeffs' in the given point 'x'
-def evaluate_polynom(coeffs, x):
+def evaluate_polynom_gorner_modulo_q(coeffs, x, q):
+    p = 0
+    for i, coeff in enumerate(coeffs):
+        p = (p * x + coeff) % q
+    return p
+
+
+def evaluate_polynom_gorner(coeffs, x):
     p = 0
     for i, coeff in enumerate(coeffs):
         p = p * x + coeff
@@ -16,7 +23,7 @@ def repr_polynomial(coeffs):
 if __name__ == '__main__':
     # coeffs = [5, 4, 1, 6]  # 5x^3 + 4x^2 + 1x^1 + 6
     coeffs = [1, 2, 3, 4, 5, 6]  # x^5 + 2x^4 + 3x^3 + 4x^2 + 5x + 6
-    res = evaluate_polynom(coeffs, 1)
+    # res = evaluate_polynom_gorner_modulo_q(coeffs, 3, 17)
+    res = evaluate_polynom_gorner(coeffs, 3)
     print(repr_polynomial(coeffs))
     print(f'val = {res}')
-
--- 02_data_structures/week_4/4.3_hash_substring/4.3_hash_substring_rabing-karp.py
+++ 02_data_structures/week_4/4.3_hash_substring/4.3_hash_substring_rabing-karp.py
@@ -1,23 +1,81 @@
-def check(s, p, pos2start):  # s - string, p - pattern to search
-    for i in range(len(p)):
-        if s[pos2start + i] != p[i]:
-            return False
-    return True
+from collections import deque
 
+class RabinKarp:
+    # q = 1000000007: prime number big enough to minimize collisions while hashing and small enough to fit into int_64
+    # x = 59: the nearest prime number to 52 as the Latin alphabet consists of 52 letters
+    def __init__(self, pattern, text, x=59, q=1000000007):
+        self.p = pattern
+        self.t = text
 
-def find_substring_brute_force(s, p):  # s - string, p - pattern to search
-    positions = []
+        self.M = len(pattern)
+        self.x = x
+        self.q = q
+        self.ms_term = 1
 
-    for pos in range(len(s) - len(p) + 1):
-        if check(s, p, pos):
-            positions.append(pos)
+        # start index of current hash window
+        self.window_start = 0
 
-    return positions
+        # end index of current hash window
+        self.window_end = self.window_start + len(self.p) - 1
+
+        self.window_hash = None
+        self.positions = []  # positions of pattern (self.p) occurrences within the text self.t
+
+        # precomputed value for the most significant term x^(M-1)mod_q
+        for i in range(self.M - 1):
+            self.ms_term = (self.ms_term * self.x) % self.q
+
+    def get_occurrences(self):
+        return self.positions
+
+    # calculates h = (ord(s[0])*x^(m-1) + ord(s[1])*x^(m-2) + ... + ord(s[m-1])*x^0)mod_q using Horner's rule
+    def str_hash(self, s, only_first_char_in_str=False):
+        coeffs = list(map(ord, s))  # coefficients of polynomial of degree (self.M - 1)
+
+        h = 0
+        for i, _ in enumerate(coeffs):
+            h = (h * self.x + coeffs[i]) % self.q
+        return h
+
+    def update_window(self):
+        self.window_start += 1
+        self.window_end += 1
+
+    def move_hash_window(self):
+        if self.window_start < len(self.t) - len(self.p):
+            first_char_hash = ord(self.t[self.window_start]) * self.ms_term
+            last_char_hash = ord(self.t[self.window_end + 1])
+            self.window_hash = ((self.window_hash - first_char_hash)*self.x + last_char_hash) % self.q
+        self.update_window()
+
+    # as it turned out it is BAD practice to implement in Python your own string comparison function
+    # !!!! always use '==' and corresponding slicing
+    def is_match(self):
+        for i, _ in enumerate(self.p):
+            if self.t[self.window_start + i] != self.p[i]:
+                return False
+        return True
+
+    def run(self):
+        pattern_hash = self.str_hash(self.p)
+        self.window_hash = self.str_hash(self.t[0:len(self.p)])
+
+        for _ in range(len(self.t) - len(self.p) + 1):
+            if self.window_hash == pattern_hash:
+                # String comparison below leads to time limit exceeded: Time used: 8.99/4.50. Who would ever suppose!!!
+                # if self.is_match():  # VERY VERY BAD !!!
+                # Check using slices and '==' works PERFECTRLY: Max time used: 0.74/4.50
+                if self.t[self.window_start:self.window_end+1] == self.p:
+                    self.positions.append(self.window_start)
+            self.move_hash_window()
 
 
 if __name__ == '__main__':
-    p = input().strip()
-    s = input().strip()
+    pattern = input().strip()
+    text = input().strip()
 
-    res = find_substring_brute_force(s, p)
-    print(*res)
+    rk = RabinKarp(pattern, text)
+    rk.run()
+    print(*rk.get_occurrences())
+
+
