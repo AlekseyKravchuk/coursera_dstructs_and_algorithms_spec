150feb247d1b81c96efc783a3867f6c990252c15
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2021-09-08 21:26:52 +0300

    Added solution to 4.5_longest_common_substring.

--- 02_data_structures/week_4/4.5_longest_common_substring/4.5_longest_common_substring.py
+++ 02_data_structures/week_4/4.5_longest_common_substring/4.5_longest_common_substring.py
@@ -3,44 +3,36 @@ from collections import namedtuple
 Query = namedtuple('Query', ['s1', 's2'])
 
 x = 29
-# p1 = 263130836933693530167218012159999999
-# p2 = 8683317618811886495518194401279999999
-
 p1 = 1000000000039
 p2 = 1000000000061
 
-# Try to pick the proper value of the buckets number
-# the number of buckets should be enough to store all substring hashes with reasonable load factor
 
-# Works, but leads to collisions
-M = 109003
-
-
-def get_substr_hashes(s, win_len, modulo):
+# calculates hashes for all possible substrings of length 'win_len' of a given string 's' by given 'modulo'
+def get_substr_hashes(s, win_len, p):
     L = len(s)
     coeffs = list(map(ord, s))
-    h = [None] * M
+    d = dict()
 
     # precomputed values of x^(str_len-1) by given modulo
-    mult = pow(x, win_len - 1, modulo)
+    mult = pow(x, win_len - 1, p)
 
     # Calculate the hash value of first window, w
     w = 0
     for i in range(win_len):
-        w = (x * w + coeffs[i]) % modulo
+        w = (x * w + coeffs[i]) % p
 
-    # h[w % M] = (start_pos_of_substring, its_lenght)
-    h[w % M] = (0, win_len)
+    # d[w] = (start_pos_of_substring, its_lenght)
+    d[w] = (0, win_len)
 
     # Calculate hashes for all substrings of length=roll_hash_win_len of a given string
     for i in range(1, L - win_len + 1):
         next_idx = i + win_len - 1
-        w = ((w - mult * coeffs[i - 1]) * x + coeffs[next_idx]) % modulo
-        h[w % M] = (i, win_len)
-    return h
+        w = ((w - mult * coeffs[i - 1]) * x + coeffs[next_idx]) % p
+        d[w] = (i, win_len)
+    return d
 
 
-def get_LCS_positions(s1, s2, ht1, ht2, ss_len, p1, p2):
+def get_LCS_positions(s2, d1, d2, ss_len, p1, p2):
     L = len(s2)
     coeffs = list(map(ord, s2))
 
@@ -53,26 +45,22 @@ def get_LCS_positions(s1, s2, ht1, ht2, ss_len, p1, p2):
     for i in range(ss_len):
         w1 = (x * w1 + coeffs[i]) % p1
         w2 = (x * w2 + coeffs[i]) % p2
-    hash1 = w1 % M  # 'hash1' the hash value, corresponds to bucket index in hash table ht1
-    hash2 = w2 % M  # 'hash2' the hash value, corresponds to bucket index in hash table ht2
-    if ht1[hash1] is not None and ht2[hash2] is not None:
-        if ht1[hash1] == ht2[hash2]:
+    if w1 in d1 and w2 in d2:
+        if d1[w1] == d2[w2]:
             # return (starting_pos_in_s1, starting_pos_in_s2, substring_len)
-            return ht1[hash1][0], 0, ss_len
+            return d1[w1][0], 0, ss_len
         else:
             return None
 
     # Calculate hashes of substrings of 'ss_len' length in string 's2' using rolling hash for 2 different modules
     for i in range(1, L - ss_len + 1):
         last_after_win_end = i + ss_len - 1
-        w1 = ((w1 - coeffs[i - 1] * mult1)*x + coeffs[last_after_win_end]) % p1
-        w2 = ((w2 - coeffs[i - 1] * mult2)*x + coeffs[last_after_win_end]) % p2
-        hash1 = w1 % M  # 'hash1' the hash value, corresponds to bucket index in hash table
-        hash2 = w2 % M  # 'hash1' the hash value, corresponds to bucket index in hash table
-        if ht1[hash1] is not None and ht2[hash2] is not None:
-            if ht1[hash1] == ht2[hash2]:
+        w1 = ((w1 - coeffs[i - 1] * mult1) * x + coeffs[last_after_win_end]) % p1
+        w2 = ((w2 - coeffs[i - 1] * mult2) * x + coeffs[last_after_win_end]) % p2
+        if w1 in d1 and w2 in d2:
+            if d1[w1] == d2[w2]:
                 # return (starting_pos_in_s1, starting_pos_in_s2, substring_len)
-                return ht1[hash1][0], i, ss_len
+                return d1[w1][0], i, ss_len
     # if we reach this place then there is no common substring of length ss_len
     return None
 
@@ -96,12 +84,13 @@ def search_LCS(s1, s2):
     while low <= high:
         mid = low + (high - low) // 2
 
+        # d1 and d2 are dictionaries: {'substring_hash':(start_pos_in_s2, substring_length)}
         # lengths[mid] is current length to be checked for LCS
-        ht1 = get_substr_hashes(s1, lengths[mid], p1)
-        ht2 = get_substr_hashes(s1, lengths[mid], p2)
+        d1 = get_substr_hashes(s1, lengths[mid], p1)
+        d2 = get_substr_hashes(s1, lengths[mid], p2)
 
         # 'lcs' tuple has the following spec: (start_index_in_s1, start_index_in_s2, substring_len)
-        lcs = get_LCS_positions(s1, s2, ht1, ht2, lengths[mid], p1, p2)
+        lcs = get_LCS_positions(s2, d1, d2, lengths[mid], p1, p2)
         if lcs is not None:
             prev_best_lcs = lcs
             low = mid + 1
