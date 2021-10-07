6d8ed7bc5719a1a630e9ccd71afea46a250f2761
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2021-10-07 13:21:24 +0300

    Working on solution for 4.6_pattern_matching_with_mismatches; Failed case #5/7: Wrong answer.

--- 00_experiments/0.31_horner_method.py
+++ 00_experiments/0.31_horner_method.py
@@ -21,14 +21,22 @@ def repr_polynomial(coeffs):
 
 
 if __name__ == '__main__':
+    x = 59
+    s = 'baab'
+    coeffs = [ord(ch) for ch in s]
+    print(f'coeffs = {coeffs}')
+    res = evaluate_polynom_gorner(coeffs, x)
+    print(res)
+
+
     # coeffs = [5, 4, 1, 6]  # 5x^3 + 4x^2 + 1x^1 + 6
     # coeffs = [1, 2, 3, 4, 5, 6]  # x^5 + 2x^4 + 3x^3 + 4x^2 + 5x + 6
     # res = evaluate_polynom_gorner_modulo_q(coeffs, 3, 17)
 
-    s = 'HellO'
-    coeffs = list(map(ord, s))  # coefficients of polynomial of degree (self.M - 1)
-    x = 263
-    p = 1000000007
-    res = evaluate_polynom_gorner_modulo_q(coeffs, x, p)
-    print(repr_polynomial(coeffs))
-    print(f'val = {res}')
+    # s = 'HellO'
+    # coeffs = list(map(ord, s))  # coefficients of polynomial of degree (self.M - 1)
+    # x = 263
+    # p = 1000000007
+    # res = evaluate_polynom_gorner_modulo_q(coeffs, x, p)
+    # print(repr_polynomial(coeffs))
+    # print(f'val = {res}')
--- 02_data_structures/week_4/4.6_pattern_matching_with_mismatches/4.6_pattern_matching_with_mismatches.py
+++ 02_data_structures/week_4/4.6_pattern_matching_with_mismatches/4.6_pattern_matching_with_mismatches.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from collections import namedtuple
-from contextlib import redirect_stdout
+
 
 # k: maximum allowed number of mismatches;
 # s: string to search in;
@@ -56,15 +56,11 @@ class PMM:
         self.ph_q1 = [[None for _ in range(N)] for _ in range(N)]
         self.ph_q2 = [[None for _ in range(N)] for _ in range(N)]
 
-        # And finally to prevent wasteful recurrent computations there is needed to store mismatch count for some
-        # overlapping in 's' and in 'p' specified by tuple (win_start, s_left, s_right)
-        self.mismatches_2D = [[None for _ in range(M)] for _ in range(M)]
-
         # calculate hashes for all possible suffixes in string 's' and pattern 'p'
         self.fill_suff_hashes()
         # *********** END of __init__ ***********
 
-    # calculate hashes of suffixes in 'self.s' and 'self.p'
+    # calculate hashes of ALL suffixes in 'self.s' and 'self.p'
     # self.sh1[i] is hash for substring s[0]s[1]s[2]...s[i-1]
     # for example:
     # s = 'trololo', len(s)=7 =>
@@ -73,7 +69,7 @@ class PMM:
         s_codes = [ord(ch) for ch in self.s]
         p_codes = [ord(ch) for ch in self.p]
 
-        # calculating hashes of all suffixes in 's' and in 'p' using Horner's rule
+        # calculating hashes of all suffixes in 's' and in 'p' using Horner's rule by 2 different modules: PMM.q1,PMM.q2
         for i, ch_code in enumerate(s_codes, start=1):
             self.s_suff_q1[i] = (PMM.x * self.s_suff_q1[i-1] + ch_code) % PMM.q1
             self.s_suff_q2[i] = (PMM.x * self.s_suff_q1[i-1] + ch_code) % PMM.q2
@@ -81,9 +77,10 @@ class PMM:
             self.p_suff_q1[j] = (PMM.x * self.p_suff_q1[j-1] + ch_code) % PMM.q1
             self.p_suff_q2[j] = (PMM.x * self.p_suff_q1[j-1] + ch_code) % PMM.q2
 
-    # set hashes of substrings specified by window start position, 'i' and 0-based left and right (INCLUSIVE) indices
-    # of overlapping substrings in 's' and in 'p'
+    # check if current overlapped regions in string 'self.s' and in pattern 'self.p' are the same
+    # overlapping region is specified by window start position 'i' and 0-based 'l' and 'r' (INCLUSIVE) indices
     # self.sh_q1[i][j] corresponds to hash of substring in 's': 's[left:right+1]' given by modulo PMM.q1
+    # self.sh_q2[i][j] corresponds to hash of substring in 's': 's[left:right+1]' given by modulo PMM.q2 and so on
     def is_overlapped_parts_equal(self, i, l, r):
         y = r - l + 1
         p_l, p_r = l - i, r - i
@@ -97,41 +94,39 @@ class PMM:
                 self.ph_q2[p_l][p_r] = self.p_suff_q2[p_r+1]
         else:
             if self.sh_q1[l][r] is None:
-                self.sh_q1[l][r] = (self.s_suff_q1[r + 1] - pow(PMM.x, y, PMM.q1) * self.s_suff_q1[l]) % PMM.q1
-                self.sh_q2[l][r] = (self.s_suff_q2[r + 1] - pow(PMM.x, y, PMM.q2) * self.s_suff_q1[l]) % PMM.q2
+                self.sh_q1[l][r] = (self.s_suff_q1[r+1] - pow(PMM.x, y, PMM.q1) * self.s_suff_q1[l]) % PMM.q1
+                self.sh_q2[l][r] = (self.s_suff_q2[r+1] - pow(PMM.x, y, PMM.q2) * self.s_suff_q2[l]) % PMM.q2
+
                 self.ph_q1[p_l][p_r] = (self.p_suff_q1[p_r+1] - pow(PMM.x, y, PMM.q1) * self.p_suff_q1[p_l]) % PMM.q1
                 self.ph_q2[p_l][p_r] = (self.p_suff_q2[p_r+1] - pow(PMM.x, y, PMM.q2) * self.p_suff_q2[p_l]) % PMM.q2
 
-        if self.sh_q1[l][r] == self.ph_q2[p_l][p_r] and self.sh_q2[l][r] == self.ph_q2[p_l][p_r]:
+        if self.sh_q1[l][r] == self.ph_q1[p_l][p_r] and self.sh_q2[l][r] == self.ph_q2[p_l][p_r]:
             return True
         else:
             return False
 
-    # count_mismatches will be invoked recursively
-    def count_mismatches(self, i, l, r, mismatches=0):
+    # count_mismatches() will be invoked recursively
+    def count_mismatches(self, i, l, r):
+        mismatches = 0
         # **** BASE CASE #1 ****
         if l > r:
             return 0
 
-        # **** BASE CASE #2****:
+        # **** BASE CASE #2 ****:
         if l == r:
             if self.s[l] == self.p[l-i]:
                 return 0
             else:
                 return 1
 
-        # **** BASE CASE #3****:
+        # **** BASE CASE #3 ****:
         flag = self.is_overlapped_parts_equal(i, l, r)
         if flag:
-            return mismatches
+            return 0
 
         # **** BASE CASE #4 ****
         if mismatches > self.k:
-            return mismatches
-
-        # **** BASE CASE #5 ****: the number of mismatches for current overlapping has been calculated recently
-        if self.mismatches_2D[l][r] is not None:
-            return self.mismatches_2D[l][r]
+            return
 
         mid = l + (r - l) // 2
         p_mid = mid-i if mid != 0 else mid
@@ -143,35 +138,70 @@ class PMM:
             if mismatches > self.k:
                 return mismatches
 
-        mismatches += self.count_mismatches(i, l, mid - 1, mismatches)
-        mismatches += self.count_mismatches(i, mid + 1, r, mismatches)
-        self.mismatches_2D[l][r] = mismatches
+        mismatches += self.count_mismatches(i, l, mid - 1)
+        # middle check to avoid unnecessary checking the right part of overlapping
+        if mismatches > self.k:
+            return mismatches
+        mismatches += self.count_mismatches(i, mid + 1, r)
         return mismatches
 
+    def handle_request_debug(self, out_file='./tests/02.out'):
+        from contextlib import redirect_stdout
+
+        # CORNER CASE:
+        if self.k >= len(self.p):
+            with open(out_file, 'a') as f:
+                with redirect_stdout(f):
+                    print(len(self.s) - len(self.p) + 1, end=' ')
+                    for num in range(len(self.s) - len(self.p) + 1):
+                        print(num, end=' ')
+                    print()
+        else:
+            for i in range(len(self.s) - len(self.p) + 1):
+                n_mismatches = self.count_mismatches(i, i, i + len(self.p) - 1)
+                if n_mismatches <= self.k:
+                    self.res.append(i)
+
+            with open(out_file, 'a') as f:
+                with redirect_stdout(f):
+                    if not self.res:
+                        print(0)
+                    else:
+                        print(len(self.res), *self.res)
+
     def handle_request(self):
-        for i in range(len(self.s) - len(self.p) + 1):
-            n_mismatches = self.count_mismatches(i, i, i+len(self.p)-1)
-            if n_mismatches <= self.k:
-                self.res.append(i)
-        # ++++++++++++++ DEBUGGING ++++++++++++++
-        with open('./tests/02.out', 'a') as f:
-            with redirect_stdout(f):
-                if not self.res:
-                    print(0)
-                else:
-                    print(len(self.res), *self.res)
-        # ++++++++++++++ END of DEBUGGING ++++++++++++++
-        # if not self.res:
-        #     print(0)
-        # else:
-        #     print(len(self.res), *self.res)
+        # CORNER CASE:
+        if self.k >= len(self.p):
+            print(len(self.s) - len(self.p) + 1, end=' ')
+            for num in range(len(self.s) - len(self.p) + 1):
+                print(num, end=' ')
+            print()
+        else:
+            for i in range(len(self.s) - len(self.p) + 1):
+                n_mismatches = self.count_mismatches(i, i, i + len(self.p) - 1)
+                if n_mismatches <= self.k:
+                    self.res.append(i)
+            if not self.res:
+                print(0)
+            else:
+                print(len(self.res), *self.res)
+
+
+def debug_solution(out_file):
+    import os
+    if os.path.exists(out_file):
+        if os.path.getsize(out_file) > 0:
+            open(out_file, 'w').close()
+
+    rqs = get_requests()
+    for n, r in enumerate(rqs):
+        pmm = PMM(r)
+        pmm.handle_request_debug()
 
 
 if __name__ == '__main__':
+    # debug_solution(out_file='./tests/02.out')
     requests = get_requests()
-
     for n, r in enumerate(requests):
         pmm = PMM(r)
         pmm.handle_request()
-    # pmm = PMM(requests[0])
-    # pmm.handle_request()
