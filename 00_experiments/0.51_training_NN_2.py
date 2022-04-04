a9ad54fa9592016aec9f606da9c20a4168a821f9
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-04-04 08:11:19 +0300

    Adding experiments.

--- 00_experiments/0.51_training_NN_2.py
+++ 00_experiments/0.51_training_NN_2.py
@@ -12,14 +12,16 @@ b = np.array([0.61273249, 1.6422662])
 # define our feed forward function
 def a1(a0):
     z = W @ a0 + b
-    # Everything else is the same though,
     return sigma(z)
 
 
-# Next, if a training example is,
-x = np.array([0.1, 0.5, 0.6])
-y = np.array([0.25, 0.75])
+if __name__ == '__main__':
+    # Next, if a training example is,
+    # x = np.array([0.1, 0.5, 0.6])
+    x = np.array([0.7, 0.6, 0.2])
+    y = np.array([0.9, 0.6])
 
-# Then the cost function is,
-d = a1(x) - y  # Vector difference between observed and expected activation
-C = d @ d  # Absolute value squared of the difference.
+    # Then the cost function is,
+    d = a1(x) - y  # Vector difference between observed and expected activation
+    C = d @ d  # Absolute value squared of the difference.
+    print(f'Cost function value: {C:.4f}')
