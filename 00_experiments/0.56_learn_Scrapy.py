cd12a202a2a0ae6fa2add2fc51350416ac98d986
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-04-27 20:46:28 +0300

    Tiny changes

--- 00_experiments/0.56_learn_Scrapy.py
+++ 00_experiments/0.56_learn_Scrapy.py
@@ -1,6 +1,6 @@
 import os
-from scrapy import Selector
-import requests
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
 
 
 if __name__ == '__main__':
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     # html = requests.get(url).content
     # sel = Selector(text=html)
 
-    f_name = os.getcwd() + '/0.56_test.html'
+    f_name = os.getcwd() + '/0.56_test4.html'
     with open(f_name, 'r') as f:
         html = f.read()
+<<<<<<< Updated upstream
         # print(html)
         selector = Selector(text=html)
 
@@ -26,3 +27,24 @@ if __name__ == '__main__':
         # data = sel_lst.extract()
         print(f'extracted data: {data}')
         print(f'the number of selectors returned: {N}')
+=======
+        selector = Selector(text=html)
+        # sel_lst = selector.xpath('//*[contains(@href, "two")]')
+
+        # Create an xpath to the href attribute
+        # sel_lst = selector.xpath('//p[@id="p2"]/a/@href')
+
+        sel_lst = selector.xpath('//a/@href')
+
+        # sel_lst = selector.xpath('//p')
+    # response = HtmlResponse(url='http://mysite.com')
+    # print(response)
+    # data = Selector(response=response).xpath('//span').extract()
+        data = sel_lst.extract()
+
+    # second_p_data = sel_lst[1].extract()
+        # data = sel_lst.extract()
+
+    print(data)
+    # print(f'second object in selector list: {data}')
+>>>>>>> Stashed changes
