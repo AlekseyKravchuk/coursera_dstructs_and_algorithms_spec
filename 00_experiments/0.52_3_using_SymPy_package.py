63a5b9922dcfca49f1f0c12fa73aaf9cc3da06a2
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-05-17 23:32:18 +0300

    Using SymPy package ...

--- 00_experiments/0.52_3_using_SymPy_package.py
+++ 00_experiments/0.52_3_using_SymPy_package.py
@@ -1,4 +1,4 @@
-"""Solution to stepik.org course: https://stepik.org/lesson/573083/step/1?unit=567632"""
+"""Solution to stepik.org course: https://stepik.org/lesson/573082/step/1?unit=567631"""
 
 # TODO: реализовать вычисление функции потерь с помощью SymPy
 
@@ -15,21 +15,15 @@ def f(x):  # ReLU function
 
 
 if __name__ == '__main__':
-    # w11, w12, w21, w22 = sp.symbols('w11, w12, w21, w22')
-    # w11, w12, w21, w22 = 0.5, -0.5, -0.5, 0.5
-    w11, w12, w21, w22 = sp.symbols('w11, w12, w21, w22')
-    # F_0_0 = 0
-    # F_0_1 = f(f(w21) + f(w22))
-    # F_1_0 = f(f(w11) + f(w12))
-    # F_1_1 = f(f(f(w11 + w21) + f(w12 + w22)))
+    w0, w1 = sp.symbols('w0, w1')
 
-    # (F(0,0) - 0)**2 = 0
-    # L = (F_0_1 - 1)**2 + (F_1_0 - 1)**2 + (F_1_1 - 0)**2
-    L = (f(f(w21) + f(w22)) - 1) ** 2 + (f(f(w11) + f(w12)) - 1) ** 2 + (f(f(f(w11 + w21) + f(w12 + w22))) - 0) ** 2
-    dL_dw11 = sp.diff(L, w11)
-    print(f'dL_dw11 = {dL_dw11}')
+    L = (-w1 + w0 - 1)**2 + (w0 - 0)**2 + (w1 + w0 - 1) ** 2 + (2*w1 + w0 - 4)**2
 
-    # print(f'F_0_0 = {F_0_0}\n'
-    #       f'F_0_1 = {F_0_1}\n'
-    #       f'F_1_0 = {F_1_0}\n'
-    #       f'F_1_1 = {F_1_1}')
+    dl_dw0 = sp.diff(L, w0)
+    dl_dw1 = sp.diff(L, w1)
+
+    print(f'dl_dw0 = {dl_dw0}')
+    print(f'dl_dw1 = {dl_dw1}')
+
+
+
