d7a73249369409356d636b0f197dafbe9f24052d
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-05-19 17:24:17 +0300

    Learning packaging in Python.

---
+++ 00_experiments/0.67_packaging.py
@@ -0,0 +1,10 @@
+# import 'utils' submodule
+import my_package.utils
+
+if __name__ == '__main__':
+    # Decide to start seing other people
+    # my_package.utils.we_need_to_talk(break_up=True)
+
+    # my_package.utils.we_need_to_talk()
+
+    my_package.we_need_to_talk()
