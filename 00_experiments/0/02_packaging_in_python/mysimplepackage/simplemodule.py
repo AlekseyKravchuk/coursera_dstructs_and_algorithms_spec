2472c6cc614a424201d1f1572a8ac4b1c017208c
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-07-18 17:51:19 +0300

    Start learning packaging from scratch.

---
+++ 00_experiments/0/02_packaging_in_python/mysimplepackage/simplemodule.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    dataScientist = set(['Python', 'R', 'SQL', 'Git', 'Tableau', 'SAS'])
+    dataEngineer = set(['Python', 'Java', 'Scala', 'Git', 'SQL', 'Hadoop'])
+
+    print(f'dataScientist: {dataScientist}')
