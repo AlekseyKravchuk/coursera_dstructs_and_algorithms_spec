dba8dc8521113de5bddc94a17922de4cc22e3e20
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2022-07-20 11:42:02 +0300

    Learn packaging in Python, datacamp course.

--- 00_experiments/0.01_structuring_imports/mysklearn/mysklearn/setup.py
+++ 00_experiments/0.01_structuring_imports/mysklearn/mysklearn/setup.py
@@ -6,8 +6,10 @@ setup(
     author="Aleksey Kravchuk",
     description="A complete package for linear regression.",
     name="mysklearn",
-    version="0.1.0",
-    packages=find_packages(include=["mysklearn", "mysklearn.*"])
+    version="0.1.0",  # (major number).(minor number).(patch number)
+    packages=find_packages(include=["mysklearn", "mysklearn.*"]),
+    install_requires=['pandas>=1.0', 'scipy==1.1', 'matplotlib>=2.2.1,<3'],
+    python_requires=['>=3.9, !=2.*']
 )
 
 # As you develop the package you will increment numbers in "version"
--- 00_experiments/0.02_packaging_in_python/mysimplepackage/simplemodule.py
+++ 00_experiments/0.02_packaging_in_python/mysimplepackage/simplemodule.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 if __name__ == '__main__':
     dataScientist = set(['Python', 'R', 'SQL', 'Git', 'Tableau', 'SAS'])
     dataEngineer = set(['Python', 'Java', 'Scala', 'Git', 'SQL', 'Hadoop'])
